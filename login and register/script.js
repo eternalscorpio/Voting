f62026c8e9097c230a147f8307fcfbc7664d595f
eternalscorpio <ilayabharathi3372@gmail.com>
2023-03-09 20:39:24 +0530

    firebase

--- login and register/script.js
+++ login and register/script.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBljkdlX7TV_7-CBO8tAS4ytXW6OrsgErE",
+    authDomain: "voting-e346d.firebaseapp.com",
+    projectId: "voting-e346d",
+    storageBucket: "voting-e346d.appspot.com",
+    messagingSenderId: "652681224630",
+    appId: "1:652681224630:web:a0468b00a12cf88903a117",
+    measurementId: "G-VQV12W5X4W"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 $(document).ready(function(){
     
     // Variables
@@ -53,3 +67,50 @@ $(document).ready(function(){
         });
     });
 });
+
+
+
+  var userData
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log(firebase.auth().currentUser);
+        userData = user;
+        loginSuccess();
+        var user1 = userData.email.split("@")[0];
+        showToast("Logged In Successfully", 5);
+    } else {
+
+    }
+});
+
+var provider1 = new firebase.auth.GoogleAuthProvider();
+
+var credential;
+
+function googleSignInPopup() {
+
+    var provider1 = new firebase.auth.GoogleAuthProvider();
+    console.log("here");
+    firebase.auth().signInWifPopup(provider1).then((result) => {
+        credential = result.credential;
+        console.log(credential["photoURL"]);
+        console.log("suc");
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log("loggedin");
+        showToast("Logged In Successfully", 5);
+    }).catch((error) => {
+        //var errorCode = error.code;
+        //var errorMessage = error.message;
+        //var email = error.email;
+        //var credential = error.credential;
+        showToast(error.message, 5);
+    });
+}
+
+
+function loginSuccess(){
+    d("userName").innerHTML = userData["displayName"];
+    d("userDP").src = userData["photoURL"];
+}
