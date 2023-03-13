d1bbb0b6dd4e07fd50424815ec4f302f224fdfb0
eternalscorpio <ilayabharathi3372@gmail.com>
2023-03-13 22:14:05 +0530

    login update

--- login and register/script.js
+++ login and register/script.js
@@ -65,7 +65,7 @@ const firebaseConfig = {
         messagingSenderId: "652681224630",
         appId: "1:652681224630:web:a0468b00a12cf88903a117",
         measurementId: "G-VQV12W5X4W"
-      };
+};
       
       // Initialize Firebase
       const app = firebase.initializeApp(firebaseConfig);
@@ -105,3 +105,73 @@ const firebaseConfig = {
         console.log(userData.uid);
         queueListener(userData)
       }
+
+var calcStat = false;
+var slideNo = 0;
+var uid = "";
+var slideUrl = "";
+
+      function signInWithEmailPassword() {
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+    
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .tan((userCredential) => {
+                console.log("Signed in");
+                loginSuccess();
+                var user = userCredential.user;
+                uid = "users/" + user["uid"];
+                d("previewWindow").src = "../quiz/live/preview.html?" + user["uid"]
+                slideStat();
+                calcControls();
+                readSlideUrl()
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                window.alert(errorMessage);
+            });
+    }
+    
+    function updateURL() {
+        var newUrl = d("url").value;
+        if (newUrl.includes("/embed?")) {
+            newUrl = newUrl.split("/embed?")[0];
+        }
+        if (newUrl.includes("/pub?")) {
+            newUrl = newUrl.split("/pub?")[0];
+        }
+        var wr = firebase.database().ref(uid + "/controls/slideurl").set(newUrl);
+    }
+    
+    function loginSuccess() {
+        d("overlay").style.display = "none";
+    }
+
+    function readSlideUrl() {
+      firebase.database().ref(uid + '/controls/slideurl').on('value', (snap) => {
+          setSlide(snap.val());
+      })
+  }
+  
+  function setSlide(valueUrl) {
+      d("url").value = valueUrl;
+      slideUrl = valueUrl;
+  }
+
+  function enableCalc(calcFlag) {
+    d("ce").innerHTML = calcFlag;
+    calcStat = calcFlag;
+    if (calcStat) {
+        d("tc").style.backgroundColor = "green"
+    }
+    else {
+        d("tc").style.backgroundColor = "red"
+    }
+}
+
+function calcControls() {
+    firebase.database().ref(uid + "/controls/calc/").on('value', (snap) => {
+        enableCalc(snap.val());;
+    })
+}
